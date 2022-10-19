493e1ce148e52153d992732c830c38119b16cb34
Houichimetarik <houichimetarik@yahoo.com>
2022-10-19 16:45:51 +0100

    "Implementation files"

--- Prototype.py
+++ Prototype.py
@@ -3,7 +3,9 @@ Name: prototype, type: creational
 Problem:
 - creating the object is very complex for the client due to the high number of parameters required and
   the complex subclassing and interface implementations
--
+- The client is required to know a lot about the object composition and characteristics in order to create it
+- the object creation is expensive in terms of time and resources
+- There are already existed similar objects ( like those that the client wants to create) in the system.
 solution:
 -
 Consequences:
