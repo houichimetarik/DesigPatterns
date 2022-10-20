e7bcf6cfbf09ec82e5d914d9369fb0f98d936bae
Houichimetarik <houichimetarik@yahoo.com>
2022-10-20 11:19:07 +0100

    "Implementation files"

--- Mediator.py
+++ Mediator.py
@@ -1,7 +1,10 @@
 """
 Name: prototype, type: Behavioral
 Problem:
--
+- The problem needs to have a lot of objects communicating with each other
+- the interdependencies among the objects is complex, unstructured and difficult to understand
+- The application's behavior is distributed among multiple classes and need to be dynamically customizable
+  but using inheritance/subclassing is impractical
 solution:
 -
 Consequences:
