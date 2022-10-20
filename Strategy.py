1b56b82934a09825f68d28b10872504f403aed3a
Houichimetarik <houichimetarik@yahoo.com>
2022-10-20 15:51:52 +0100

    "Implementation files"

--- Strategy.py
+++ Strategy.py
@@ -1,32 +1,33 @@
 """
 Name: Strategy or Policy, type: Behavioral
 Problem:
-- We are building a complex system which have has to deal with a dynamic problem  that needs more that one algorithm to be implemented
-- the choice of the algorithm to use depends heavily on the client request -> Different algorithms will be appropriate at different times
+- We are building a complex system which has to deal with a dynamic problem that needs more that one algorithm to be used
+- the choice of the algorithm to use depends heavily on the client request -> Different algorithms will be needed
+  at different times.
 - We need to hard code all the needed algorithms in the part of code used to handle the client's request
-- Everytime we need add a new algorithm or maintain some of them, we need to break the client code and modify it
+- Everytime we need to add a new algorithm or maintain some of them, we need to break the client code.
 solution:
 - encapsulate every algorithm in a separate class, the encapsulated algorithm is then called a concretestrategy
-- make a common interface (we call strategy) to all (implemented by) classes that encapsulate the desired algorithms
-- The client ( we call it the context ) can call the algorithms dynamically ( when it needs to) via the interface
-- if the client (context) needs its data to be accessed then it can provide an interface to the Strategy
+- make a common interface (called "strategy") available for all the (implemented by) classes that encapsulate the desired algorithms
+- The client ( we call it here "context" ) can call the algorithms dynamically ( when it needs to) via the interface
+- if the client (context) needs its data to be accessed then it can provide an interface to the Strategy class.
 Consequences:
 - the algorithms can be easily reused
-- The algorithms can be easily upgraded and enriched using inheritence
+- The algorithms can be easily upgraded and enriched using inheritance
 - the client's code remain clean and easily maintained
-- we can implement different behavior of the same algorithm
+- we can implement different behavior of the same algorithm.
 - we can easily eliminate if-else and switch statements in the client's code
 Drawbacks:
 - the client must understand the different strategies so he can call the appropriate one
 - we increase the number of objects in the design
 - the unneeded amount of communication between the strategy interface and the context: sometimes the client use initiate the
   interface with all needed parameters in order to use a simple concrete strategy that does not need all that data and parameters
-  to, avoid this, we need to increase coupling between the client and strategy which is a bad practise
+  to, avoid this, we need to increase coupling between the client and strategy which is a bad practice.
 """
 """implementation 
 - As an example we are going to assume that a client needs different type of sorting algorithms
 - We are going implement the sorting algorithms as concrete strategies
-- Now are going to implement a common interface of the concrete strategies
+- we implement a common interface of the concrete strategies
 """
 
 """The Strategy interface declares operations common to all supported versions
